a85960923e0caac4a0627cb460c874575dd40b79
Sebastian Maier <sebastian.maier@comci.de>
2014-11-02 00:05:07 +0100

    cleanup

--- src/main/java/de/comci/bitmap/BitMapDimension.java
+++ src/main/java/de/comci/bitmap/BitMapDimension.java
@@ -11,7 +11,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
